3f39053dea052548fd8ffdee31d48409c59eeae1
Lauren B. <ljboykins@owu.edu>
2025-12-09 13:33:38 -0500

    Update treenode.cpp

--- treenode.cpp
+++ treenode.cpp
@@ -4,47 +4,53 @@
 
 using namespace std;
 
-//allocate space for n children and their names
+//space for n children and their names
 void TreeNode::Set_Num_Children(int n){
-	children.resize(n);
-	child_names.resize(n);
+    children.resize(n);
+    child_names.resize(n);
 }
 
-//return the number of children
+//returns the number of children
 int TreeNode::Get_Num_Children() const{
-	return children.size();
+    return children.size();
 }
 
-//set the branch name for child at position pos 
+//sets the  branch name for child at index pos
 void TreeNode::Set_Child_Name(int pos, string new_name){
-	child_names[pos] = new_name;
+    child_names[pos] = new_name;
 }
 
-//get the branch name for child at position pos
-string TreeNode::Set_Child_Name(int pos, string new_name){
-	return child_names[pos];
+// get branch name for child at index pos
+string TreeNode::Get_Child_Name(int pos) const{
+    return child_names[pos];
 }
 
-//set pointer to child at position pos 
-void TreeNode::Set_Child(int pos, Tree* T){
-	children[pos] = T;
+// set pointer to child at index pos
+void TreeNode::Set_Child(int pos, TreeNode* T){
+    children[pos] = T;
 }
 
-//get pointer to child at pos
+// get pointer to child at index pos
 TreeNode* TreeNode::Get_Child(int pos) const{
-	return children[pos];
+    return children[pos];
 }
 
-//mark this node as leaf if node if leaf
+//mark node as leaf or not a leaf
+void TreeNode::Set_Leaf(bool leaf){
+    is_leaf = leaf;
+}
+
+//checks if it is a leaf
 bool TreeNode::Is_Leaf() const{
-	return is_leaf;
+    return is_leaf;
 }
 
-//set the info string (attribute name or class label)
+//setting info string (attribute name or class label)
 void TreeNode::Set_Info(string new_info){
-	info = new_info;
+    info = new_info;
 }
-//get the info string
+
+//get info string 
 string TreeNode::Get_Info() const{
-	return info;
+    return info;
 }
